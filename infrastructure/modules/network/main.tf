1bf186226d4f985ca5010946de0b1da8064a8756
Arthur Pineau <arthupineau04@gmail.com>
2024-11-30 17:37:36 +0100

    feat(Infrastucture): move files in infrastructure, create network module and test

--- infrastructure/modules/network/main.tf
+++ infrastructure/modules/network/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "main" {
 
 #Vnet creation
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.Vnet_name
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
