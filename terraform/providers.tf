2ab779e2c23a75c7a6ed39aeb546ebe16e39d094
Arthur Pineau <arthupineau04@gmail.com>
2024-11-30 16:37:06 +0100

    Creation of Vnet, Subnet, Ip public, Gateway
    add your subscription id in the provider

---
+++ terraform/providers.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  subscription_id = ""
+  
+    features {}
+}
+
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
